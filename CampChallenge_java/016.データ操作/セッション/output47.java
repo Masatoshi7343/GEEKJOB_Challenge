5b92412dc535b1c470b0a32c4ec7f49a759dfa82
Masatoshi7343 <https://github.com/Masatoshi7343/GEEKJOB_Challenge.git>
2018-06-02 20:38:29 +0900

    データ操作　セッション課題　”セッションの作成”　提出

---
+++ CampChallenge_java/016.データ操作/セッション/output47.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kadai12;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+/**
+ *
+ * @author guest1Day
+ */
+public class output47 extends HttpServlet {
+
+    /**
+     *セッションを利用して現在時刻を記録してください。
+     * また，２回目以降のアクセス時は，セッションの値を取り出し，前回にアクセスした時刻の情報を画面に表示してください。
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+             Date nowtime =new Date();//現時刻の作成
+            SimpleDateFormat time= new SimpleDateFormat("yyyy年MM月dd日 HH時mm分ss秒LLにログイン");//時刻の値作成
+           String T= time.format(nowtime);//現時刻の値を作成
+           HttpSession se=request.getSession();
+           
+           se.setAttribute("time", T);
+           out.print( se.getAttribute("time"));
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
